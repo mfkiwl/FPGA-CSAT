4f061976af3c967578148c40115c849b351b98c3
Erin Cold <coldegret@protonmail.com>
2023-02-13 17:51:53 -0500

    params: settling on max 24 literals per clause
    This was what gave best perf (vs 32 and 16) on a reasonable UNSAT suite

--- shared/src/shared_parameters.hpp
+++ shared/src/shared_parameters.hpp
@@ -3,9 +3,9 @@
 constexpr unsigned int LUT_SIZE = 8;
 constexpr unsigned int TRUTH_TABLE_BITS = 1 << LUT_SIZE;
 
-constexpr unsigned int MAX_GATES = 1024;
+constexpr unsigned int MAX_GATES = 16384;
 constexpr unsigned int MAX_OCCURRENCES = MAX_GATES * (LUT_SIZE + 1);
 
 constexpr unsigned int MAX_LEARNED_CLAUSES = 16382;
 constexpr unsigned int RESERVED_NODE_IDS = 2;
-constexpr unsigned int MAX_LITERALS_PER_CLAUSE = 32;
+constexpr unsigned int MAX_LITERALS_PER_CLAUSE = 24;
